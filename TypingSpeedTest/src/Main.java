685888ed5377d0215df79c382f2c8c073f184255
giteneaxharau <enea.xharau@cit.edu.al>
2022-04-14 15:08:05 -0400

    Version 2.0 - Revised GUI, revised Comments

--- TypingSpeedTest/src/Main.java
+++ TypingSpeedTest/src/Main.java
@@ -1,33 +1,55 @@
 import javax.swing.*;
-import javax.swing.text.BadLocationException;
-import javax.swing.text.DefaultHighlighter;
-import javax.swing.text.Highlighter;
+import javax.swing.text.*;
 import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        //Gui initializing
+        //GUI INITIALIZING AND STYLING
         JPanel panel = new JPanel();
         JFrame frame = new JFrame("Typing Speed Test");
         JTextPane textPane = new JTextPane();
         JButton startGame = new JButton("Start Test");
+        JLabel title = new JLabel();
         Highlighter highlighter;
+        Font font = new Font("Bondon", Font.PLAIN, 20);
+        Font fontTitle = new Font("Bondon", Font.PLAIN, 25);
 
         panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
-        panel.setLayout(new GridLayout(2, 1, 20, 30));
-        panel.setPreferredSize(new Dimension(840, 400));
-        panel.add(textPane);
-        panel.add(startGame);
+        panel.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        panel.setPreferredSize(new Dimension(1280, 720));
+        gbc.gridx = 2;
+        gbc.gridy = 1;
+        gbc.anchor = GridBagConstraints.PAGE_START;
+        panel.add(title, gbc);
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.ipady = 400;
+        gbc.ipadx = 1000;
+        gbc.gridwidth = 3;
+        gbc.anchor = GridBagConstraints.CENTER;
+        gbc.insets = new Insets(70,10 ,0,10);
+        panel.add(textPane, gbc);
+        gbc.ipady = 30;
+        gbc.ipadx = 80; //reset to default
+        gbc.weighty = 1.0;   //request any extra vertical space
+        gbc.anchor = GridBagConstraints.PAGE_END; //bottom of space
+        gbc.insets = new Insets(10,0,110,0);  //top padding
+        gbc.gridx = 1;
+        gbc.gridwidth = 2;
+        gbc.gridy = 2;
+        panel.add(startGame, gbc);
+        panel.setBackground(new Color(173, 232, 244));
+
+
         frame.add(panel);
         frame.add(panel, BorderLayout.CENTER);
         frame.pack();
@@ -35,15 +57,22 @@ public class Main {
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        startGame.setBorder(BorderFactory.createLineBorder(Color.GRAY,2,true));
 
-        var ref = new Object() { // global object with all variables used by all components
+        title.setText("Typing Speed Test");
+        title.setFont(fontTitle);
+        title.setForeground(Color.BLACK);
+
+
+        var ref = new Object() { //GLOBAL OBJECT WITH ALL THE VARIABLES NEEDED
             ArrayList<String> words = null;
             List<String> targetWords = null;
             long startTime;
             String placeholder;
         };
 
-        try { //file reading and words populating
+        //FILE READING AND POPULATING
+        try {
             FileReader fr = new FileReader("TypingSpeedTest/src/wordlist.txt");
             BufferedReader bfr = new BufferedReader(fr); //read file
             ref.words = new ArrayList<>();
@@ -54,35 +83,44 @@ public class Main {
             e.printStackTrace();
         }
 
-        //button functionality
+        //JButton FUNCTIONALITY
         startGame.addActionListener(actionEvent -> {
-            //take input about number of words by user
+            //TAKES THE INPUT FROM USER TO SET THE AMOUNT OF WORDS ON THE JTextPane
             int wordCount = Integer.parseInt(JOptionPane.showInputDialog(frame, "Enter the number of words", JOptionPane.PLAIN_MESSAGE));
-            ref.startTime = System.currentTimeMillis(); //start stopwatch
-            Collections.shuffle(ref.words); // shufle the words
-            ref.targetWords = ref.words.subList(0, wordCount); // create target text and set it to the pane
+            ref.startTime = System.currentTimeMillis(); //STARTS TIME
+            Collections.shuffle(ref.words); //SHUFFLES THE WORDS SO IT IS RANDOM
+            ref.targetWords = ref.words.subList(0, wordCount); //CREATES THE TARGET TEXT AND SETS IT
             ref.placeholder = String.join(" ", ref.targetWords);
             textPane.setText(ref.placeholder);
             textPane.setFocusable(true);
             textPane.requestFocus();
         });
 
-        //text pane validator and keylisteners
         textPane.setEditable(false);
         highlighter = textPane.getHighlighter();
         textPane.setHighlighter(highlighter);
+
+        //CENTERING THE TEXT HORIZONTALLY FOR THE JTextPane
+
+        StyledDocument doc = textPane.getStyledDocument();
+        SimpleAttributeSet center = new SimpleAttributeSet();
+        StyleConstants.setAlignment(center, StyleConstants.ALIGN_CENTER);
+        doc.setParagraphAttributes(0, doc.getLength(), center, false);
+        textPane.setFont(font);
+        textPane.setBorder(BorderFactory.createLineBorder(new Color(0, 180, 216), 4, true));
+        textPane.setBackground(new Color(255, 255, 250));
+
         textPane.addKeyListener(new KeyAdapter() {
-            int rightWords = 0; //when 10 show joption and end program or reset
+            int rightWords = 0;
             int spacePresses = 0;
             String input = "";
             @Override
             public void keyPressed(KeyEvent e) {
                 System.out.println();
                 super.keyTyped(e);
-                //space listener
+                //LISTENER FOR SPACES
                 if (e.getKeyCode() == KeyEvent.VK_SPACE){
                     spacePresses += 1;
-                    //textPane.setCaretPosition(textPane.getCaretPosition()+1);
                     String targetWord = ref.targetWords.get(spacePresses - 1);
                     String lastInput = input.split(" ")[input.split(" ").length -1];
                     if (targetWord.equals(lastInput)) rightWords++;
@@ -93,11 +131,12 @@ public class Main {
                                 " in "+ (timeInSeconds/ 60.00) + "minutes. " + "Means your speed is: " +
                                 Math.round(rightWords/(timeInSeconds/60))+ " WPM(words per minute).";
                         JOptionPane.showMessageDialog(frame, result, "Results: ", JOptionPane.INFORMATION_MESSAGE);
-                        System.exit(0); //just close the window and reset textpane text
+                        System.exit(0); //CLOSES THE WHOLE PROGRAM
                     }
-                }else {   // any character listener
+                }else {   //LISTENER FOR ANY CHARACTER
                     char ch = e.getKeyChar();
                     if (Character.isAlphabetic(ch)) input += ch;
+                    //HIGHLIGHTER IMPLEMENTATION
                     if (ref.placeholder.charAt(input.length()-1) != ch) {
                         try {
                             highlighter.addHighlight(input.length()-1, input.length(),
@@ -114,7 +153,7 @@ public class Main {
                         }
                     }
 
-                } // add highlighter using caretPosition -1
+                }
             }
         });
     }
